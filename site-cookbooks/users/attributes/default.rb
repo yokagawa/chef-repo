7108898199ac83dcdb9a241c6d52db079825538d
yuuya okagawa <yuuya.dev@gmail.com>
2015-08-05 16:17:34 +0900

    first commit

---
+++ site-cookbooks/users/attributes/default.rb
@@ -0,0 +1,11 @@
+# ユーザ追加
+default['users']['add'] = [
+  # { name: 'ユーザ名', password: 'パスワード' }
+  { name: 'yokagawa', password: 'yokagawa' }
+]
+
+# ユーザ削除
+default['users']['delete'] = [
+  # { name: 'ユーザ名'}
+  { name: 'ec2-user'}
+]
---
+++ solo.rb
@@ -0,0 +1,2 @@
+base = File.expand_path('..', __FILE__)
+cookbook_path [base + '/cookbooks', base + '/site-cookbooks']
